cadb61cc03b218e1b0ee0c969be16e7dfe3f1f35
Liuxy <liuxy0551@qq.com>
2019-05-05 20:57:40 +0800

    optimize

--- scripts/auto_backup.js
+++ scripts/auto_backup.js
@@ -24,49 +24,47 @@ function run() {
 
     } else {
 
-        echo("======================Auto Backup Begin===========================");
+        echo("============================Auto Backup Begin============================");
 
         cd('/Users/liuxy/Desktop/Projects/Blog/liuxy0551.github.io');    //此处修改为Hexo根目录路径
 
         if (exec('git add -A').code !== 0) {
 
-            echo('Error: Git add failed');
+            echo('Error: git add failed');
 
             exit(1);
 
         } else {
 
-          echo('Success: Git add success');
+          echo('Success: git add success');
 
         }
 
         if (exec('git commit -m "optimize"').code !== 0) {
 
-            echo('Error: Git commit failed');
-
-            echo(exec('git commit -m "optimize"').code);
+            echo('Error: git commit failed');
 
             exit(1);
 
         } else {
 
-          echo('Success: Git commit success');
+          echo('Success: git commit success');
 
         }
 
         if (exec('git push origin develop').code !== 0) {
 
-            echo('Error: Git push failed');
+            echo('Error: git push failed');
 
             exit(1);
 
         } else {
 
-          echo('Success: Git push success');
+          echo('Success: git push success');
 
         }
 
-        echo("==================Auto Backup Complete============================")
+        echo("============================Auto Backup Complete============================")
 
     }
 
